fdaab65012c63867c99c12517f9c57ed44dba37b
nakul <nakul0579.be23@chitkara.edu.in>
2024-03-24 15:32:06 +0530

    Added the required files for electron js

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app,BrowserWindow,globalShortcut,dialog,Tray,Menu,ipcMain}=require('electron')
+function createWindows() {
+    const window=new BrowserWindow({
+        width:1200,
+        height:600,
+        title:"Hang Man Game",
+        // frame:false,
+        // alwaysOnTop:true,
+        // fullscreen:true,
+        webPreferences:{
+            nodeIntegration:true,
+            contextIsolation:false,
+            enableRemoteModule:true
+        },
+        resizable:false,
+        icon:__dirname +'./static/Hangman-icon.png'
+       
+        
+    });
+    // Menu.setApplicationMenu(menu)
+    // const child=new BrowserWindow({parent:window})
+    // window.webContents.openDevTools()
+    window.maximize();
+    window.loadFile("./Game_files/begin.html")
+   
+   
+}
+app.on('ready',()=>{
+    console.log("SuccessFully App Started");
+    createWindows();
+})
